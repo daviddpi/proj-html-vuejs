355b4efb5460a0ca8f267e1dfa50ed6c61176c7f
daviddpi <piscopodavid97@gmail.com>
2021-10-08 15:42:02 +0200

    completata parte css del footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <footer>
         <div class="container">
-            <div class="row pb-5 text-white">
-                <div class="col-12 col-md-3">
+            <div class="row text-white">
+                <div class="col-12 col-md-3 logo-footer">
                     <img class="img-fluid" src="../assets/img/footer-logo-1.png" alt="logo iAcademy">
                     <ul>
                         <li>text</li>
@@ -12,26 +12,26 @@
                     </ul>
                     <div>
                         <i class="fas fa-phone-alt"></i>
-                        tel
+                        <span>Tel</span>
                     </div>
                     <div>
                         <i class="far fa-clock"></i>
-                        ora
+                        <span>ora</span>
                     </div>
                 </div>
                 <div class="col-12 col-md-3">
                     <h2>Popular Courses</h2>
                     <ul>
                         <li>
-                            <h5>Engh</h5>
+                            <h6>Engh</h6>
                             <p>Mar</p>
                         </li>
                         <li>
-                            <h5>Social</h5>
+                            <h6>Social</h6>
                             <p>David</p>
                         </li>
                         <li>
-                            <h5>Spani</h5>
+                            <h6>Spani</h6>
                             <p>Jennie</p>
                         </li>
                     </ul>
@@ -48,7 +48,32 @@
                 </div>
                 <div class="col-12 col-md-3">
                     <h2>Flex</h2>
-                    <img class="img-fluid" src="../assets/img/footer-img-1.png" alt="">
+                    <ul>
+                        <li>
+                            <img class="img-fluid" src="../assets/img/footer-img-1.png" alt="">
+                        </li>
+                    </ul>
+                </div>
+                <hr>
+                <div class="col-12 sub-footer">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            Copyright 2017 &copy;Mikado Themes
+                        </div>
+                        <div class="social">
+                            <div>Call +44 300 303 0266</div>
+                            <div>Follow Us</div>
+                            <div>
+                                <i class="fab fa-twitter"></i>
+                            </div>
+                            <div>
+                                <i class="fab fa-instagram"></i>
+                            </div>
+                            <div>
+                                <i class="fab fa-facebook-f"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -69,6 +94,65 @@ export default {
 footer{
     background-color: #222222;
     padding-top: 100px;
+
+    h2, h6{
+        font-family: $primaryFont;
+    }
+
+    ul{
+        list-style: none;
+        padding-top: 35px;
+        padding-left: 0;
+    }
+    
+    .col-12, .col-md-3{
+        padding-bottom: 70px;
+    }
+
+    li{
+        margin-bottom: 15px;
+    }
+    
+    hr{
+        border: 1px solid #414141;
+    }
+
+    .sub-footer{
+        color: #949494;
+        padding: 20px 0;
+
+        .social{
+            width: 35%;
+            display: flex;
+            justify-content: space-between;
+            text-transform: uppercase;
+        }
+    }
 }
+
+.logo-footer{
+    img{
+        height: 38px;
+    }
+
+    ul li{
+        margin-bottom: 3px;
+    }
+
+    li:last-child{
+        margin-bottom: 25px;
+    }
+
+    .fas{
+        margin-bottom: 25px;
+    }
+
+    span{
+        padding-left: 15px;
+    }
+}
+
+
+
 
 </style>
